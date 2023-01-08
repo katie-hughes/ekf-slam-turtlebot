ec1476eb95368de6f8d99ff8c46db0eb4ba4feb5
Katie Hughes <katiereeshughes@gmail.com>
2023-01-08 14:36:14 -0600

    fixing some syntax in the launchfile

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -1,8 +1,6 @@
 
 """
-Launches rviz with my turtlebot urdf file.
-Launchfile format taken from this ros tutorial:
-https://github.com/ros/urdf_tutorial/blob/ros2/launch/display.launch.py
+Launches rviz with the turtlebot3 urdf file.
 """
 
 from launch import LaunchDescription
@@ -16,22 +14,18 @@ from launch.conditions import LaunchConfigurationEquals
 
 def generate_launch_description():
     pkg_share = get_package_share_path('nuturtle_description')
-    urdf_path = pkg_share / 'turtle.urdf.xacro'
+    urdf_path = pkg_share / 'turtlebot3_burger.urdf'
     rviz_config_path = pkg_share / 'turtle_urdf.rviz'
-    rviz_config_path_base_fixed = pkg_share / 'turtle_urdf_base_fixed.rviz'
 
-    jsp_arg = DeclareLaunchArgument(name='use_jsp', default_value='gui',
-                                    choices=['gui', 'jsp', 'none'],
-                                    description='Choose which joint state publisher to run')
+    jsp_arg = DeclareLaunchArgument(name='use_jsp', default_value='true',
+                                    choices=['true', 'false'],
+                                    description='Choose if joint_state_publisher is launched')
     model_arg = DeclareLaunchArgument(name='model', default_value=str(urdf_path),
                                       description='Absolute path to robot urdf file')
     rviz_arg = DeclareLaunchArgument(name='rvizconfig',
-                                     default_value=str(rviz_config_path_base_fixed),
-                                     choices=[str(rviz_config_path),
-                                              str(rviz_config_path_base_fixed)],
+                                     default_value=str(rviz_config_path),
                                      description='Absolute path to rviz config file')
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
-                                       value_type=str)
+    robot_description = ParameterValue(LaunchConfiguration('model'), value_type=str)
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -39,24 +33,11 @@ def generate_launch_description():
         parameters=[{'robot_description': robot_description}]
     )
 
-    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
-    # (or neither if it's none)
+    # Launch JSP if use_jsp is true.
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        # if the jsp argument is jsp
-        # I copied the format from here:
-        # https://answers.ros.org/question/360264/ifconditionpythonexpression-am-i-doing-it-right/
-        condition=LaunchConfigurationEquals('use_jsp', 'jsp')
-    )
-
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        # if the jsp argument is gui
-        # again format copied from here:
-        # https://answers.ros.org/question/360264/ifconditionpythonexpression-am-i-doing-it-right/
-        condition=LaunchConfigurationEquals('use_jsp', 'gui')
+        condition=LaunchConfigurationEquals('use_jsp', 'true')
     )
 
     rviz_node = Node(
@@ -71,7 +52,6 @@ def generate_launch_description():
         model_arg,
         rviz_arg,
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node,
     ])
