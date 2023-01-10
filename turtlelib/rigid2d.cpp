79a344832f2e9fd01909999050ede34b24626d97
Katie Hughes <katiereeshughes@gmail.com>
2023-01-10 12:11:05 -0600

    ideas for first cpp functions

--- turtlelib/rigid2d.cpp
+++ turtlelib/rigid2d.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include "rigid2d.hpp"
 
+namespace turtlelib
+{
+    // help: https://www.learncpp.com/cpp-tutorial/printing-inherited-classes-using-operator/
+    std::ostream & operator<<(std::ostream & os, const Vector2D & v){
+        os << " [" << v.x << "] ";
+        os << " [" << v.y << "] ";
+        return os;
+    }
+
+    std::istream & operator>>(std::istream & is, Vector2D & v){
+        return is;
+    }
+}
+
 
 
