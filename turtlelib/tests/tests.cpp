8c41b3edb1091f33ac49d458ff76ea20d01df4a9
Katie Hughes <katiereeshughes@gmail.com>
2023-01-13 15:39:41 -0600

    Getting started on tests

--- turtlelib/tests/tests.cpp
+++ turtlelib/tests/tests.cpp
@@ -1,8 +1,10 @@
 #include "turtlelib/rigid2d.hpp"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    turtlelib::Transform2D Ttest = {turtlelib::Vector2D{2,3}, 45};
+TEST_CASE( "Rotation", "transform" ) {
+    turtlelib::Transform2D Ttest1 = {turtlelib::Vector2D{2,3}, 45};
+    turtlelib::Transform2D Ttest2 = {turtlelib::Vector2D{2,3}, -45};
     REQUIRE( 1  == 1 );
-    REQUIRE( Ttest.rotation() == 45);
+    REQUIRE( Ttest1.rotation() == 45);
+    REQUIRE( Ttest2.rotation() == -45);
 }
