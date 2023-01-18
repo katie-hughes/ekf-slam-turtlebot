db879742793bd69fae24de00d1130228a640f25e
Katie Hughes <katiereeshughes@gmail.com>
2023-01-18 11:09:39 -0600

    reset service worksgit add --all

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -18,15 +18,14 @@ class Nusim : public rclcpp::Node
     Nusim()
     : Node("nusim"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~timestep", 10);
+      publisher_ = this->create_publisher<std_msgs::msg::UInt64>("timestep", 10);
+
       timer_ = this->create_wall_timer(
         200ms,
         std::bind(&Nusim::timer_callback, this));
 
-      rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_;
-
       reset_ = this->create_service<std_srvs::srv::Empty>(
-        "~reset",
+        "reset",
         std::bind(&Nusim::reset, this, std::placeholders::_1, std::placeholders::_2));
     }
 
@@ -39,13 +38,19 @@ class Nusim : public rclcpp::Node
       publisher_->publish(message);
       count_++;
     }
-    void reset(std_srvs::srv::Empty::Request::SharedPtr req,
-                      std_srvs::srv::Empty::Response::SharedPtr res){
+
+    void reset(std::shared_ptr<std_srvs::srv::Empty::Request> req,
+               std::shared_ptr<std_srvs::srv::Empty::Response> res){
+        RCLCPP_INFO_STREAM(get_logger(), "Resetting!");
         count_ = 0;
     }
 
     rclcpp::TimerBase::SharedPtr timer_;
+
     rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
+
+    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_;
+
     size_t count_;
 };
 
