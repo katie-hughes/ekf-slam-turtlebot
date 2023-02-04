603e71b51cf655ceff4f1d48e9bdee62bba17d1e
Katie Hughes <katiereeshughes@gmail.com>
2023-02-03 20:16:41 -0600

    add throwing exceptions

--- nuturtle_control/src/odometry.cpp
+++ nuturtle_control/src/odometry.cpp
@@ -26,24 +26,55 @@ class Odometry : public rclcpp::Node
     Odometry()
     : Node("turtle_control")
     {
-      // TODO Figure out how to have these as uninitialized ;-;
-      // https://docs.ros2.org/foxy/api/rclcpp/classrclcpp_1_1Node.html#a095ea977b26e7464d9371efea5f36c42
-      // this->declare_parameter("body_id","");
-      // this->declare_parameter("odom_id","odom");
-      // this->declare_parameter("wheel_left","");
-      // this->declare_parameter("wheel_left","");
+      this->declare_parameter("body_id","base_footprint");
+      // this->declare_parameter("body_id",body_id);
+      body_id = this->get_parameter("body_id").as_string();
+      RCLCPP_INFO_STREAM(get_logger(), "Body ID: "<<body_id);
+      if (body_id.empty()){
+         throw std::logic_error("Invalid body_id!");
+      }
 
-      // wheel_radius = this->get_parameter("wheel_radius").as_double();
-      // track_width = this->get_parameter("track_width").as_double();
-      // encoder_ticks = this->get_parameter("encoder_ticks_per_rad").as_double();
+      // default = odom
+      this->declare_parameter("odom_id","odom");
+      odom_id = this->get_parameter("odom_id").as_string();
+      RCLCPP_INFO_STREAM(get_logger(), "Odom ID: "<<odom_id);
 
-      // RCLCPP_INFO_STREAM(get_logger(), "Wheel Radius: "<<wheel_radius);
-      // RCLCPP_INFO_STREAM(get_logger(), "Track Width: "<<track_width);
-      // RCLCPP_INFO_STREAM(get_logger(), "Encoder Ticks: "<<encoder_ticks);
+      this->declare_parameter("wheel_left","wheel_left_joint");
+      // this->declare_parameter("wheel_left",wheel_left);
+      wheel_left = this->get_parameter("wheel_left").as_string();
+      RCLCPP_INFO_STREAM(get_logger(), "Wheel Left: "<<wheel_left);
+      if (wheel_left.empty()){
+         throw std::logic_error("Invalid wheel_left!");
+      }
+
+      this->declare_parameter("wheel_right","wheel_right_joint");
+      // this->declare_parameter("wheel_right",wheel_right);
+      wheel_right = this->get_parameter("wheel_right").as_string();
+      RCLCPP_INFO_STREAM(get_logger(), "Wheel Right: "<<wheel_right);
+      if (wheel_right.empty()){
+         throw std::logic_error("Invalid wheel_right!");
+      }
+
+      this->declare_parameter("wheel_radius",-1.0);
+      wheel_radius = this->get_parameter("wheel_radius").as_double();
+      RCLCPP_INFO_STREAM(get_logger(), "Wheel Radius: "<<wheel_radius);
+      if (wheel_radius<0.0){
+        throw std::logic_error("Invalid wheel_radius!");
+      }
+
+      this->declare_parameter("track_width",-1.0);
+      track_width = this->get_parameter("track_width").as_double();
+      RCLCPP_INFO_STREAM(get_logger(), "Track Width: "<<track_width);
+      if (wheel_radius<0.0){
+        throw std::logic_error("Invalid track_width!");
+      }
 
       // slightly hacky workaround to get new values in
-      // turtlelib::DiffDrive temp(track_width, wheel_radius);
-      // robot = temp;
+      turtlelib::DiffDrive temp(track_width, wheel_radius);
+      robot = temp;
+
+      // udpate header fields of the curr_odom object. 
+      // ie .header.stamp, .header.frame_id, .child_frame_id
 
       odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("odom", 10);
       
@@ -61,13 +92,33 @@ class Odometry : public rclcpp::Node
   private:
     void timer_callback()
     {
-      RCLCPP_INFO_STREAM(get_logger(), "Current Pose: "<<current_pose);
+      RCLCPP_INFO_STREAM(get_logger(), "Current Pose: "<<robot.get_config());
+      // publish odometry message based on current config.
+      current_odom.header.stamp = this->get_clock()->now();
+      current_odom.pose.pose.position.x = robot.get_x();
+      current_odom.pose.pose.position.y = robot.get_y();
+      // then there is also a quaternion i need to edit...
+      odom_pub_->publish(current_odom);
     }
 
     void js_cb(const sensor_msgs::msg::JointState & js)
     {
-      (void) js;
       RCLCPP_INFO_STREAM(get_logger(), "JS Received");
+      if (!first_iteration){
+        double dl = js.position[0] - last_js.position[0];
+        double dr = js.position[1] - last_js.position[1];
+        RCLCPP_INFO_STREAM(get_logger(), "dl: "<<dl);
+        RCLCPP_INFO_STREAM(get_logger(), "dr: "<<dr);
+        double current_s = js.header.stamp.sec + 1e-9*js.header.stamp.nanosec;
+        double last_s = last_js.header.stamp.sec + 1e-9*last_js.header.stamp.nanosec;
+        double dt = current_s - last_s;
+        RCLCPP_INFO_STREAM(get_logger(), "dt: "<<dt);
+        // apply forward kinematics
+        robot.fk(dl/dt, dr/dt);
+      } else {
+        first_iteration = false;
+      }
+      last_js = js;
     }
 
     void initial_pose(
@@ -80,7 +131,8 @@ class Odometry : public rclcpp::Node
       /// \param Response: boolean, if relocation is successful
       RCLCPP_INFO_STREAM(get_logger(), "Service Call!");
       turtlelib::Transform2D new_pose(turtlelib::Vector2D{req->x, req->y},req->theta);
-      current_pose = new_pose;
+      turtlelib::DiffDrive new_robot(new_pose, track_width, wheel_radius);
+      robot = new_robot;
       res->success = true;
     }
 
@@ -91,8 +143,13 @@ class Odometry : public rclcpp::Node
 
     rclcpp::Service<nuturtle_control::srv::InitialPose>::SharedPtr initial_pose_srv_;
 
-    turtlelib::Transform2D current_pose;
-
+    // initialize with garbage values, overwrite later
+    turtlelib::DiffDrive robot{0.0, 0.0};
+    double wheel_radius, track_width;
+    sensor_msgs::msg::JointState last_js;
+    bool first_iteration = true;
+    nav_msgs::msg::Odometry current_odom;
+    std::string body_id, odom_id, wheel_left, wheel_right;
 };
 
 int main(int argc, char * argv[])
